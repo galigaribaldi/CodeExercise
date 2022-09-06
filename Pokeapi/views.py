44fea477c1f345abbcb443c96aa3500d81f97c76
galigaribaldi <hcabrera@e-bitware.com.mx>
2022-09-06 01:40:08 -0500

    Add Documentation

--- Pokeapi/views.py
+++ Pokeapi/views.py
@@ -154,4 +154,4 @@ class PokemonTeamOnly_APIView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
